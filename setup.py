1469cc149a712cdcb012be63b00e33939ccc4c91
Yuri Lya <yuri-l@users.noreply.github.com>
2014-07-31 11:49:33 +1100

    Changed ver.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-fs-trumbowyg',
-    version='0.1.3',
+    version='0.1.4',
     packages=['trumbowyg'],
     include_package_data=True,
     install_requires=[],
